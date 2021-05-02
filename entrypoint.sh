9b4cdb4773fe820bb66801348041cfac78f23f7b
Mayank Gupta <mkgupta74d@gmail.com>
2021-05-02 13:00:24 +0200

    Github action pyinstaller with python3.7

---
+++ entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Fail on errors.
+set -e
+
+# Make sure .bashrc is sourced
+. /root/.bashrc
+
+# Allow the workdir to be set using an env var.
+# Useful for CI pipiles which use docker for their build steps
+# and don't allow that much flexibility to mount volumes
+SRCDIR=$1
+
+PYPI_URL=$2
+
+PYPI_INDEX_URL=$3
+
+WORKDIR=${SRCDIR:-/src}
+
+SPEC_FILE=${4:-*.spec}
+
+TYPE=win32
+
+python -m pip install --upgrade pip wheel setuptools
+
+#
+# In case the user specified a custom URL for PYPI, then use
+# that one, instead of the default one.
+#
+if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
+   [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
+    # the funky looking regexp just extracts the hostname, excluding port
+    # to be used as a trusted-host.
+    mkdir -p /wine/drive_c/users/root/pip
+    echo "[global]" > /wine/drive_c/users/root/pip/pip.ini
+    echo "index = $PYPI_URL" >> /wine/drive_c/users/root/pip/pip.ini
+    echo "index-url = $PYPI_INDEX_URL" >> /wine/drive_c/users/root/pip/pip.ini
+    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /wine/drive_c/users/root/pip/pip.ini
+
+    echo "Using custom pip.ini: "
+    cat /wine/drive_c/users/root/pip/pip.ini
+fi
+
+cd $WORKDIR
+
+if [ -f $5 ]; then
+    pip install -r $5
+fi # [ -f $5 ]
+
+
+pyinstaller --clean -y --dist ./dist/windows/$TYPE --workpath /tmp $SPEC_FILE
+chown -R --reference=. ./dist/windows/$TYPE
+
+
+apt-get install -y file
+
+ls ./dist/windows/$TYPE | echo "::set-output name=location::$WORKDIR/dist/windows/$TYPE/$(< /dev/stdin)"
+ls ./dist/windows/$TYPE | echo "::set-output name=filename::$(< /dev/stdin)"
+
+echo "::set-output name=content_type::$(ls ./dist/windows/$TYPE | file --mime-type ./dist/windows/$TYPE/$(< /dev/stdin) | awk '//{ print $2 }')"
