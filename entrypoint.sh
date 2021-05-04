588a258ef13f260c00c0acce5aeda04e3d6c7f25
Mayank Gupta <mkgupta74d@gmail.com>
2021-05-04 13:43:29 +0200

    updated and bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,7 +69,7 @@ then
     mv $FILE_DIR/$DEF_FILE_NAME $FILE_DIR/$RENAME
     ls $FILE_DIR | echo "::set-output name=location::$WORKDIR/$FILE_DIR/$(< /dev/stdin)"
     ls $FILE_DIR | echo "::set-output name=filename::$DEF_FILE_NAME"
-    echo "::set-output name=content_type::$(ls ./dist/linux/$TYPE | file --mime-type $FILE_DIR/$(< /dev/stdin) | awk '//{ print $2 }')"
+    echo "::set-output name=content_type::$(ls $FILE_DIR | file --mime-type $FILE_DIR/$(< /dev/stdin) | awk '//{ print $2 }')"
 else
     ls $FILE_DIR | echo "::set-output name=location::$WORKDIR/$FILE_DIR"
     ls $FILE_DIR | echo "::set-output name=filename::NULL"
