1ab1db5666c90bf0c03ce993549f2c072e8c43af
Mayank Gupta <mkgupta74d@gmail.com>
2021-05-05 20:22:09 +0200

    updated and bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -53,8 +53,6 @@ fi # [ -f requirements.txt ]
 
 chown -R --reference=. $FILE_DIR
 
-apt-get install -y file
-
 FILES_COUNT=`ls $FILE_DIR | wc -l`
 
 if [ $FILES_COUNT = 1 ]
@@ -73,7 +71,7 @@ if [ $FILES_COUNT = 1 ]
 then
     echo "::set-output name=location::$WORKDIR/$FILE_DIR/$RENAME"
     echo "::set-output name=filename::$RENAME"
-    echo "::set-output name=content_type::$(ls $FILE_DIR | file --mime-type $FILE_DIR/$(< /dev/stdin) | awk '//{ print $2 }')"
+    echo "::set-output name=content_type::$(file --mime-type $FILE_DIR/$RENAME | awk '//{ print $2 }')"
 else
     echo "::set-output name=location::$WORKDIR/$FILE_DIR"
     echo "::set-output name=filename::NULL"
